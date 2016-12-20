2baf2cf708919989cf69c15fe4077b8f705af3ec
Samuel Hapák <samuel.hapak@gmail.com>
2016-12-20 20:10:42 +0100

    Upload excludes files found in .docsignore file.
    If the target folder of `yarn run upload` contains `.docsignore` file, use it as
    a list of files not to be included in the resulting archive.

--- upload.js
+++ upload.js
@@ -1,12 +1,28 @@
 import archiver from 'archiver'
 import fs from 'mz/fs'
 import path from 'path'
+import {run} from 'yacol'
 
-function upload(folder) {
+function* ignore(folder) {
+  const ignoreFile = path.join(folder, '.docsignore')
+  const parse = (f) => f.split('\n')
+      .map((s) => s.trim())
+      .filter((s) => !s.match(/^#/) && s !== '')
+
+  return (yield fs.stat(ignoreFile)).isFile()
+      ? parse(yield fs.readFile(ignoreFile, 'utf-8'))
+      : []
+}
+
+function* upload(folder) {
   const output = fs.createWriteStream(path.join(__dirname, 'example.zip'))
   const archive = archiver('zip')
   archive.pipe(output)
-  archive.glob('**/*', {dot: true, cwd: folder})
+  archive.glob('**/*', {
+    dot: true,
+    cwd: folder,
+    ignore: yield run(ignore, folder)
+  })
   archive.finalize()
 }
 
@@ -16,4 +32,4 @@ if (!folder) {
   console.log(`Usage: yarn run upload <folder-to-upload>`)
   process.exit(0)
 }
-upload(process.argv[2])
+run(upload, process.argv[2])
