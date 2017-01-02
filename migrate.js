cf50d379f71f9ca2dc34a96b9c9e5d978026d873
Samuel Hapák <samuel.hapak@gmail.com>
2017-01-02 13:19:11 +0100

    Use request.js in migrate.js.

--- migrate.js
+++ migrate.js
@@ -1,36 +1,9 @@
-import p from 'path'
 import {run} from 'yacol'
-import url from 'url'
-import http from 'http'
-import https from 'https'
 import parseArgs from 'minimist'
 import transenv from 'transenv'
+import request from './request.js'
 
 const apiKey = transenv()(({str}) => str('API_KEY'))
-
-function request(u, subPath, method) {
-  const {protocol, hostname, port, path} = url.parse(u)
-  const headers = {'Authorization': apiKey}
-  const requestFn = {'http:': http.request, 'https:': https.request}[protocol]
-  if (requestFn === undefined) throw new Error(`Invalid protocol ${protocol}`)
-
-  let resolve, reject
-
-  const response = new Promise((res, rej) => {
-    resolve = res
-    reject = rej
-  })
-
-  const request = requestFn({
-    host: hostname,
-    port,
-    path: p.join(path, subPath),
-    headers,
-    method
-  }, resolve)
-
-  return {request, response}
-}
 
 function error(url, result) {
   if (result.statusCode !== 200) {
@@ -39,16 +12,16 @@ function error(url, result) {
 }
 
 function *migrate(from, to) {
-  const backup = request(from, '$backup', 'GET')
-  const restore = request(to, '$restore', 'PUT')
+  const [breq, bres] = request(from, {path: '$backup', method: 'GET', apiKey})
+  const [rreq, rres] = request(to, {path: '$restore', method: 'PUT', apiKey})
 
-  backup.request.end()
-  const bResponse = yield backup.response
-  bResponse.pipe(restore.request)
-  bResponse.on('end', () => restore.request.end())
+  breq.end()
+  const bResponse = yield bres
+  bResponse.pipe(rreq)
+  bResponse.on('end', () => rreq.end())
 
   error(from, bResponse)
-  error(from, yield restore.response)
+  error(from, yield rres)
 
   console.log(`Succesfully migrated from ${from} to ${to}`)
 }
