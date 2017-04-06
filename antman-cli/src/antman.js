2bff04c24466c9c9d683849154db893198700aba
Samuel Hapák <samuel.hapak@gmail.com>
2017-04-06 16:31:27 +0200

    Add init command to antman.
    This enables you to easily create new handbook repositories.

--- antman-cli/src/antman.js
+++ antman-cli/src/antman.js
@@ -2,9 +2,11 @@ import {run} from 'yacol'
 import parseArgs from 'minimist'
 import {upload as _upload, link as _link} from './upload.js'
 import {migrate as _migrate} from './migrate.js'
+import {init as _init} from './init.js'
 
 const usageUpload = 'antman upload [-a] -k api_key host folder'
 const usageMigrate = 'antman migrate -k api_key -f from_url -t to_url'
+const usageInit = 'antman init name'
 
 function upload(args) {
   const host = args['_'][1]
@@ -34,13 +36,22 @@ function migrate(args) {
   })
 }
 
+function init(args) {
+  const name = args['_'][1]
+  if (!name) {
+    console.log(`usage: ${usageInit}`)
+    process.exit(0)
+  }
+  _init(name)
+}
+
 const args = parseArgs(process.argv.slice(2), {
   alias: {'alias': 'a', 'from': 'f', 'to': 't', 'api-key': 'k'},
   'boolean': ['alias'],
 })
 
 const command = args['_'][0]
-const action = {upload, migrate}[command]
+const action = {upload, migrate, init}[command]
 
 if (action) action(args)
-else console.log(`usage: ${usageUpload}\n${usageMigrate}`)
+else console.log(`usage: ${usageUpload}\n${usageMigrate}\n${usageInit}`)
---
+++ antman-cli/src/init.js
@@ -0,0 +1,11 @@
+import {exec} from 'child_process'
+import fs from 'fs'
+
+export function init(name) {
+  const initRepo = 'git@github.com:vacuumlabs/init-handbook.git'
+  if (fs.existsSync(name)) {
+    console.error(`Directory ${name} already exists.`)
+    process.exit(1)
+  }
+  exec(`git clone --depth=1 --branch=master ${initRepo} ${name} && rm -rf !$/.git`)
+}
