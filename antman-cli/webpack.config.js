71804f0dcf86166922549c566ea7649ffb538c8e
michal-lenik <miso.lenik@gmail.com>
2019-07-23 12:39:09 +0200

    Upgrade antman packages

--- antman-cli/webpack.config.js
+++ antman-cli/webpack.config.js
@@ -3,6 +3,7 @@ var path = require('path')
 var nodeExternals = require('webpack-node-externals')
 
 module.exports = {
+  mode: 'production',
   entry: path.join(__dirname, 'src/antman.js'),
   target: 'node',
   externals: nodeExternals(),
