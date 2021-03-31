eae5a87815e2aee6e8eeb54da73c8f0a45ca27f9
Samuel Hapák <samuel.hapak@gmail.com>
2021-03-31 16:50:40 +0200

    Merge delivery menus.

--- src/menu.js
+++ src/menu.js
@@ -8,18 +8,23 @@ export default [
     link: '/vacuum-book/',
   },
   {
+    title: 'Delivery (tech, design, product)',
+    description:
+      'Daily life of engineers and designers, client communication, hiring process, and more.',
+    icon: 'fas fa-laptop-code',
+    submenu: [
+      ['Delivery Process', '/vacuum-book/delivery-process/'],
+      ['Interviewing Engineers', '/assessment/'],
+      ['Estimating Projects', '/estimation-book/'],
+      ['Coding Tips', '/coding-tips/'],
+    ],
+  },
+  {
     title: 'Management Tips',
     description:
       'Vacuumlabs Management tips, tricks, guidelines, and best practices.',
     icon: 'fas fa-tasks',
     link: '/management-tips/',
-  },
-  {
-    title: 'IT Delivery (PM Handbook)',
-    description:
-      'Guidelines concerning our IT project sales & delivery. Must read for all sales, PMs, RMs, tech leads, finance, and legal people.',
-    icon: 'fas fa-tasks',
-    link: '/pm-book/',
   },
   {
     title: 'Finance Handbook',
@@ -38,28 +43,6 @@ export default [
     description: 'Vacuumlabs brand manual.',
     icon: 'fas fa-bullhorn',
     link: '/marketing-design/',
-  },
-  {
-    title: 'Assessment and Interview',
-    description:
-      'Set of guides, tips, and interview questions for our technical assessments.',
-    icon: 'fa fa-user-plus',
-    //updatedAt: '20160307T14:23:21',
-    link: '/assessment/',
-  },
-  {
-    title: 'Project Estimation',
-    description: 'How we estimate projects at Vacuumlabs.',
-    icon: 'fa fa-clock',
-    link: '/estimation-book/',
-  },
-  {
-    title: 'Coding Tips',
-    description:
-      'This public handbook provides useful tips and tricks for programmers.',
-    icon: 'fa fa-code',
-    //updatedAt: '20160207T11:39:21',
-    link: '/coding-tips/',
   },
   {
     title: 'Tech Handbook',
